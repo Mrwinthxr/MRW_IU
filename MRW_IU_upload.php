ce6807d00dfd1bcc59e0d759d2b19b557cf40924
unknown <mrwinther@live.dk>
2016-01-31 02:10:26 +0100

    starting to look like something

--- MRW_IU_upload.php
+++ MRW_IU_upload.php
@@ -1,22 +1,25 @@
 <html>
-<head>	
+<head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="dropzone.css">
-	<link rel="stylesheet" type="text/css" href="mrw_iu_css.css">
+	<title> Billed Upload </title>
+
+	<link rel="stylesheet" type="text/css" href="css/dropzone.css">
+	<link rel="stylesheet" type="text/css" href="css/mrw_iu_css.css">
 </head>
 
 <body>
-	<div id="image_upload">
-		<h1>Upload Siden</h1>
-		
-		<div id="drop_zone">
-	    <form action="upload.php" class="dropzone">
-	    </form>
-		</div>
+	<div id="img-up_bck">
+		<div id="image_upload">
 
+			<div id="drop_zone">
+		    <form action="upload.php" class="dropzone">
+		    </form>
+			</div>
+
+		</div>
 	</div>
-	<script type="text/javascript" src="dropzone.js"></script>
-	<script type="text/javascript" src="mrw_iu_js.js"></script>
+	<script type="text/javascript" src="js/dropzone.js"></script>
+	<script type="text/javascript" src="js/mrw_iu_js.js"></script>
 </body>
 
 </html>		
--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 <head>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="mrw_iu_css.css">
+	<link rel="stylesheet" type="text/css" href="css/mrw_iu_css.css">
 </head>
 
 <body>
 	<h1>Show pictures</h1>
 
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
-	<script type="text/javascript" src="mrw_iu_js.js"></script>
+	<script type="text/javascript" src="js/mrw_iu_js.js"></script>
 </body>
 
 </html>		
